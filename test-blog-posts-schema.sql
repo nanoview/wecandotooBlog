541a1643c34bd23da1e57d3bac8bc785e0b28bb9
nanoview <arif.js@gmail.com>
2025-08-22 15:56:24 +0300

    CLEANUP: Remove unnecessary documentation, SQL, and test files from repository

--- test-blog-posts-schema.sql
+++
@@ -1,5 +0,0 @@
--- Simple test to check if we can access blog_posts and see what columns exist
-SELECT column_name 
-FROM information_schema.columns 
-WHERE table_name = 'blog_posts' 
-ORDER BY ordinal_position;
